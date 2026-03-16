aeed2e17eb2e9d38a23f63f1266a9650efe63ab6
Alberto Mozos <albmozos@amazon.com>
2026-03-16 21:54:15 +0100

    Added ada credentials as part of deployment

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "=== Obteniendo credenciales AWS ==="
+ada credentials update --account=537090272297 --provider=conduit --role=IibsAdminAccess-DO-NOT-DELETE --once
+
 echo "=== Buildeando frontend ==="
 cd frontend
 npm run build
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "=== Obteniendo credenciales AWS ==="
+ada credentials update --account=537090272297 --provider=conduit --role=IibsAdminAccess-DO-NOT-DELETE --once
+
 export CDK_DOCKER=finch
 
 echo "=== Buildeando backend ==="
