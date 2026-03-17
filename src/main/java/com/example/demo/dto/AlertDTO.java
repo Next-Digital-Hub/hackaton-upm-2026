33750baee2cbf1f9d8328e856cecd8a9745bad9e
daniirivv <danielriveranunezdearenas@outlook.com>
2026-03-17 10:40:26 +0100

    task: implement AlertDTO

--- src/main/java/com/example/demo/dto/AlertDTO.java
+++ src/main/java/com/example/demo/dto/AlertDTO.java
@@ -1,4 +1,15 @@
 package com.example.demo.dto;
 
-public record AlertDTO() {
-}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public record AlertDTO(
+        String indicativo,
+        String nombre,
+        String provincia,
+        String fecha,
+        String tmax, // Temperatura máxima (viene como "26,0")
+        String tmin, // Temperatura mínima
+        String prec, // Precipitación
+        String hrMedia // Humedad relativa media
+) {}
