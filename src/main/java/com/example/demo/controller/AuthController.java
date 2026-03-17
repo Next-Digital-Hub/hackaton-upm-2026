25bf9fb08fbdba3f024fd4d5af1413a68324b35e
marcossanchezz <m.srobles@alumnos.upm.es>
2026-03-17 11:27:41 +0100

    Controlador de autentificacion, y encriptacion de contraseña

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserRegitrationDto;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("registrar")
+    public ResponseEntity<String> registrar(@RequestBody UserRegitrationDto dto){
+        try {
+            userService.registrarCiudadano(dto);
+            return ResponseEntity.ok("Ciudadano regitrado");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Error en el registro: "+ e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/com/example/demo/controller/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
+                        .permitAll().anyRequest().authenticated());
+        return http.build();
+    }
+}
