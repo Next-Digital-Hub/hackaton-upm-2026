800e9432bf3156ec71b6afe11947a0dd02e1eee9
SergioRubio <s.rtriguero@alumnos.upm.es>
2026-03-17 15:31:36 +0100

    weatherProvide implementation

---
+++ campusSostenible/src/main/java/com/kernelpanic/campusostenible/core/providers/weather/WeatherProviderImpl.java
@@ -0,0 +1,11 @@
+package com.kernelpanic.campusostenible.core.providers.weather;
+
+import java.util.List;
+
+import com.kernelpanic.campusostenible.core.domain.WeatherData;
+
+public class WeatherProviderImpl implements WeatherProvider{
+    public List<WeatherData> getTodayWeather(){
+        return null;
+    }
+}
