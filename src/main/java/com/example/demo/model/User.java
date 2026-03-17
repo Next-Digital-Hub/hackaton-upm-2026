7e7090f0356821abf1c7f5ca38414f1c6b1d6bcf
marcossanchezz <m.srobles@alumnos.upm.es>
2026-03-17 11:25:00 +0100

    Añadido User y su clases hijas y enunms para el creado de los user

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,18 +5,78 @@ import jakarta.persistence.*;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "rol-usuario")
-public abstract class User {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(nullable = false)
     private String nombre;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String provincia; //Enum con provincias????
 
+    @Column(nullable = false)
     private TipoVivienda tipoVivienda;
 
+    @Column(nullable = false)
+    private TipoNecesidades tipoNecesidades;
 
+    @Column(nullable = false)
+    private Rol rol;
 
+    public long getId() {
+        return id;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public TipoVivienda getTipoVivienda() {
+        return tipoVivienda;
+    }
+
+    public void setTipoVivienda(TipoVivienda tipoVivienda) {
+        this.tipoVivienda = tipoVivienda;
+    }
+
+    public TipoNecesidades getTipoNecesidades() {
+        return tipoNecesidades;
+    }
+
+    public void setTipoNecesidades(TipoNecesidades tipoNecesidades) {
+        this.tipoNecesidades = tipoNecesidades;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
