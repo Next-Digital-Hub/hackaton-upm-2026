5f8dc953bf2c9cea1157715464bf1aac22a471cf
rochaenrique <rochabenattienrique@gmail.com>
2026-03-17 17:50:56 +0100

    last minute changes

--- bmf-clima/src/app/AppSidebar.tsx
+++ bmf-clima/src/app/AppSidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { ChevronDown } from "lucide-react"
 import {
   Sidebar,
   SidebarContent,
@@ -7,9 +7,44 @@ import {
   SidebarGroup,
   SidebarHeader,
   SidebarProvider,
+  SidebarMenuButton,
+  SidebarMenu,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import React from "react";
 import { useRouter } from "next/navigation";
+
+const fakeHistory = [
+                     "Weekly Rainfall Summary Report",
+                     "Real-Time Wind Speed Alerts",
+                     "Temperature Trends Over the Last 30 Days",
+                     "Severe Storm Warning Analysis",
+                     "Daily Humidity Level Updates",
+                     "Air Pressure Fluctuation Report",
+                     "Heatwave Monitoring Dashboard",
+                     "Snowfall Accumulation Tracker",
+                     "Lightning Strike Activity Log",
+                     "Seasonal Climate Comparison Insights",
+                     "UV Index Risk Notifications",
+                     "Fog Density and Visibility Report",
+                     "Hurricane Path Prediction Updates",
+                     "Drought Conditions Assessment",
+                     "Microclimate Data Overview"
+                     ];
 
 export default function AppSidebar() {
   const router = useRouter();
@@ -22,11 +57,31 @@ export default function AppSidebar() {
           <Sidebar className="bg-primary" variant="inset">
           <SidebarHeader>Sidebar header</SidebarHeader>
           
-          <SidebarContent>
-          <SidebarGroup>History of Chats</ SidebarGroup>
           <SidebarGroup>Meteorology </ SidebarGroup>
           <SidebarGroup onClick={handleClick}>Crear Emergencia </ SidebarGroup>
+          
+          <SidebarContent>
+          <SidebarMenu>
+          <SidebarMenuItem>
+          <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+          <SidebarMenuButton>
+          History of Chats
+          <ChevronDown className="ml-auto" />
+          </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+          
+          {fakeHistory.map((h) => <DropdownMenuItem key={h}><span>{h}</span></ DropdownMenuItem>)}
+          
+          </DropdownMenuContent>
+          </DropdownMenu>
+          </SidebarMenuItem>
+          </SidebarMenu>
+          
+          
           </ SidebarContent>
+          
           
           <SidebarFooter>BMF-Clima</SidebarFooter>
           
