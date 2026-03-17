5aa076cd82bc4dd90dade2da82ca58884db1e3a3
daniirivv <danielriveranunezdearenas@outlook.com>
2026-03-17 13:24:55 +0100

    add: UserPromptVO.java

---
+++ src/main/java/com/example/demo/controller/UserPromptVO.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.antlr.v4.runtime.misc.NotNull;
+
+public record UserPromptVO(
+        String generalGuidance,
+        String previsionContext,
+        String userContext
+) {
+
+    @Override
+    public String toString(){
+        String template = """
+                %s
+                
+                **Previsión**
+                %s
+                
+                **Contexto del usuario**
+                %s
+                """;
+
+        return template.formatted(generalGuidance, previsionContext, userContext);
+    }
+}
