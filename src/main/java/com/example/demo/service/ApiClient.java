fdb3732adcdc02d03afa2094c3cf3802751bffae
daniirivv <danielriveranunezdearenas@outlook.com>
2026-03-17 10:50:27 +0100

    refactor: rename builder

--- src/main/java/com/example/demo/service/ApiClient.java
+++ src/main/java/com/example/demo/service/ApiClient.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.AlertDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -10,19 +11,19 @@ public class ApiClient {
 
     // Spring inyecta el RestClient.Builder y también busca el valor de hackaton.api.jwt
     public ApiClient(
-            RestClient.Builder builder,
+            RestClient.Builder restBuilder,
             @Value("${hackaton.api.jwt}") String token) {
 
-        this.restClient = builder
+        this.restClient = restBuilder
                 .baseUrl("https://api.github.com/Next-Digital-Hub/hackaton-upm-2026")
                 .defaultHeader("Authorization", "Bearer " + token) // Usamos la variable inyectada
                 .build();
     }
 
-    public AlertaMeteorologica[] obtenerAlertasDesdeApi() {
+    public AlertDTO getAlert() {
         return this.restClient.get()
                 .uri("/alertas-meteorologicas")
                 .retrieve()
-                .body(AlertaMeteorologica[].class);
+                .body(AlertDTO.class);
     }
 }
